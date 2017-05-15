997ec5ea849da06a3e69bf051835681823a1c7c9
Matthew Nelson <matthewnelson18@gmail.com>
2017-05-15 16:07:43 -0700

    updated user model to have rstrnt/movie favs

--- models/User.js
+++ models/User.js
@@ -3,7 +3,19 @@ const
   userSchema = new mongoose.Schema({
     username: String,
     email: String,
-    zipCode: Number
+    zipCode: Number,
+    movies: [movieSchema],
+    restaurants: [restuarantSchema]
+  }),
+  restuarantSchema = mongoose.Schema({
+    name: String,
+    rating: Number,
+    phoneNumber: Number,
+    cuisine: String
+  }),
+  movieSchema = mongoose.Schema({
+    name: String,
+    genre: String
   })
 
 module.exports = mongoose.model('User', userSchema)
