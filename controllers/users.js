59474777a7ccc027bf7b84798c8c4be1e7aba503
Matthew Nelson <matthewnelson18@gmail.com>
2017-05-15 13:29:48 -0700

    changed users routes, added user crud methods

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,51 @@ const
   User = require('../models/User.js')
 
 module.exports = {
+
+  //we will be removing this later, we wont have a users index page
+  index: (req,res) => {
+    User.find({}, (err, usersFromDb) => {
+      if(err) return console.log(err)
+      res.render('index', {users: usersFromDb})
+    })
+  },
+
   show: (req, res) => {
-    User.find({}, (err, users) => {
+    User.findById(req.params.id, (err, users) => {
       if(err) return console.log(err)
       res.render('show', {users: users})
+    })
+  },
+
+  new: (req, res) => {
+    res.render('new')
+  },
+
+  create: (req, res) => {
+    User.create(req.body, (err, user) => {
+      if(err) return console.log(err)
+      res.redirect('/users/' + user.id)
+    })
+  },
+
+  edit: (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+      if(err) return console.log(err)
+      res.render('edit', {user: user})
+    })
+  },
+
+  update: (req, res) => {
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, user) => {
+      if(err) return console.log(err)
+      res.redirect('/users/' + user.id)
+    })
+  },
+
+  destroy: (req, res) => {
+    User.findByIdAndDestroy(req.params.id, (err, user) => {
+      if(err) return console.log(err)
+      res.redirect('/')
     })
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,18 @@ const
   usersController = require('../controllers/users.js')
 
 
+usersRouter.route('/')
+  .get(usersController.index) //we will remove this later
+  .post(usersController.create)
+
+usersRouter.get('/new', usersController.new)
+usersRouter.get('/:id/edit', usersController.new)
+
+
 usersRouter.route('/:id')
   .get(usersController.show)
+  .patch(usersController.update)
+  .delete(usersController.destroy)
+
 
 module.exports = usersRouter
