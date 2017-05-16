268c3a5a1eb8703d609116541f383d629c3f71c2
Matthew Nelson <matthewnelson18@gmail.com>
2017-05-16 16:07:37 -0700

    trimmed routes

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,6 @@ const
   usersController = require('../controllers/users.js')
 
 
-usersRouter.route('/')
-  .get(usersController.index) //we will remove this later using to test crud
-  .post(usersController.create)
-
-usersRouter.get('/new', usersController.new)
-usersRouter.get('/:id/edit', usersController.edit)
 usersRouter.get('/:id/favorites', usersController.favorites)
 
 usersRouter.route('/:id')
