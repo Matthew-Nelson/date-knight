48be330379ce7cdc182f8f07dee80d9c6dd69f4c
K.Dicke <k.dicke7@gmail.com>
2017-05-16 23:08:02 -0700

    yelp and cinepass apis added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const
   express = require('express'),
   app = express(),
   mongoose = require('mongoose'),
+  yelp = require('yelp-fusion')
   logger = require('morgan'),
   bodyParser = require('body-parser'),
   usersRoutes = require('./routes/users.js'),
@@ -9,10 +10,14 @@ const
   ejsLayouts = require('express-ejs-layouts'),
   methodOverride = require('method-override'),
   mongoDB = ('mongodb://localhost/date-knight'),
+  CinepassAPI = require('cinepass-api'),
+  findango = require('findango-api'),
+  clientId = 'B8KK0dTkGvmwwQl-4EsWfA',
+  clientSecret = 'C7mlUNxCQ2QsE1PmeMLJGkz5J1ITcL0rAtoEP7CjBw814U7eld6emqSmSPkVSnTp',
   PORT = 3000
 
 mongoose.connect(mongoDB, (err) => {
-  console.log( err || "Connected to mongo DB!")
+  console.log(err || "Connected to mongo DB!")
 })
 
 app.set('view engine', 'ejs')
@@ -26,12 +31,31 @@ app.use(bodyParser.urlencoded()) //
 
 
 app.get('/', (req, res) => {
-  res.render('index')
+  var zipCode = '90401'
+  var foodType = 'bar'
+  const searchRequest = {
+    term: foodType,
+    location: zipCode,
+    radius: 10000,
+    open_now: true
+  };
+  yelp.accessToken(clientId, clientSecret).then(response => {
+    const client = yelp.client(response.jsonBody.access_token);
+    client.search(searchRequest).then(response => {
+      const searchResult = response.jsonBody.businesses;
+      CinepassAPI.init('CcRgdWOMlbM7xoRfx4S3LKrkumTA2tip')
+      CinepassAPI.getMovies({lat:34.0522, log:118.2437}, (movies)=>{
+        // console.log(movies[1]);
+        res.render('index', {searchResult: searchResult, movies: movies})
+      })
+    });
+  }).catch(e => {console.log(e);});
+
 })
 
 app.use('/users', usersRoutes)
 
 
 app.listen(PORT, (err) => {
-  console.log( err || "Server running on port", PORT )
+  console.log(err || "Server running on port", PORT)
 })
